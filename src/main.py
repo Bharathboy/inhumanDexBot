486de9fcb4445e26545835a9f2276ff8fe724d97
Bharathboy <84450562+Bharathboy@users.noreply.github.com>
2022-04-10 09:09:23 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -94,7 +94,7 @@ def ptype(app, message):
     except IndexError as s:
         app.send_message(
             chat_id=message.chat.id,
-            text=" where is type?\nuse eg '`/type poison'`"
+            text=" where is type?\nUse eg:`/type poison`"
         )
         return
     try:
