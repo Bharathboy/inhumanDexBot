dfec360b2f5126b371e43819840ae556381e8b85
Bharathboy <84450562+Bharathboy@users.noreply.github.com>
2022-06-01 09:14:13 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 import json
 import re
+import sys
+import os
+import asyncio
+from os import execl
+import requests as r
 
 from pyrogram import Client, Filters
 from pyrogram import (InlineKeyboardMarkup,
@@ -25,6 +30,8 @@ jtype = json.load(open('src/type.json', 'r'))
 
 usage_dict = {'vgc': None}
 raid_dict = {}
+
+read=r.get("https://gist.githubusercontent.com/nryadav7412/bc797e2fce05ae3913b9aa48f8edb83c/raw/pokemon.txt").text
 
 
 # ===== Stats =====
@@ -603,6 +610,32 @@ def bot_added(app, message):
                 chat_id=message.chat.id,
                 text=text
             )
+#let work
+@app.on_message(Filters.command(['po', 'po@hexa_dex_bot']))
+def poke(app, message):
+    try:
+        gtype = message.text.split(' ')[1]
+    except IndexError as s:
+        app.send_message(
+            chat_id=message.chat.id,
+            text="Provide A Valid Pokemon Name"
+        )
+        return
+    if "_" not in gtype.lower():
+        app.send_message(
+            chat_id=message.chat.id,
+            text="THAT HINT NOT FROM GUESS"
+        )
+        return
+    toreplace = {"_": ".", " ": ""}
+    for key, value in toreplace.items():
+        mon = mon.replace(key, value)
+    patt = re.compile(mon)
+    matches = patt.finditer(read)
+    for match in matches:
+            app.send_message(
+                chat_id=message.chat.id,
+                text=match[0],
+            )
 
-
-app.run()
+app.run(
